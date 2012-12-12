6017ba7eb487f11101c1704e39f927fdf92a5eac
vidya <vsagarj@gmail.com>
2012-12-12 08:50:10 -0800

    cleanup code

--- scheduler.rb
+++ scheduler.rb
@@ -2,28 +2,28 @@
 #
 # Write methods in the Scheduler class to generate the following output
 #
-#   (after 1 sec) time is now 1 seconds
-#   (after 2 sec) time is now 2 seconds
-#   (after 3 sec) time is now 3 seconds
+#	 (after 1 sec) time is now 1 seconds
+#	 (after 2 sec) time is now 2 seconds
+#	 (after 3 sec) time is now 3 seconds
 #
 # from the program:
 #
-#       class Scheduler
-#       end
+#			 class Scheduler
+#			 end
 
-#       Scheduler.after 3 do
-#       	puts "time is now 3 seconds"
-#       end
+#			 Scheduler.after 3 do
+#			 	puts "time is now 3 seconds"
+#			 end
 
-#       Scheduler.after 2 do
-#       	puts "time is now 2 seconds"
-#       end
+#			 Scheduler.after 2 do
+#			 	puts "time is now 2 seconds"
+#			 end
 
-#       Scheduler.after 1 do
-#       	puts "time is now 1 seconds"
-#       end
+#			 Scheduler.after 1 do
+#			 	puts "time is now 1 seconds"
+#			 end
 
-#       Scheduler.run
+#			 Scheduler.run
 #
 
 require 'pry'
@@ -32,20 +32,20 @@ class Scheduler
 	@@jobs = []
 
 	def self.run
-    @@jobs.sort_by! { |job| job[0] }
+		@@jobs.sort_by! { |job| job[0] }
 
-    current_time = 0
+current_time = 0
 
 		@@jobs.each do |start_time, handler|
 
-      puts "before call: current_time = #{Time.now}"
-      sleep(start_time - current_time)
-      handler.call
-      puts "after call: current_time = #{Time.now}"
-      puts
+puts "before call: current_time = #{Time.now}"
+sleep(start_time - current_time)
+handler.call
+puts "after call: current_time = #{Time.now}"
+puts
 
-      current_time = start_time
-    end
+current_time = start_time
+end
 	end
 
 	def self.after(period, &handler)
@@ -66,7 +66,7 @@ Scheduler.after 2 do
 end
 
 Scheduler.after 1 do
-  puts "time is now 1 seconds"
+	puts "time is now 1 seconds"
 end
 
 Scheduler.run
